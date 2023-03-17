d8f44edc723ee5663c2e223bb93aba7f8b9c6fec
NielAnasco <122077231+NielAnasco@users.noreply.github.com>
2023-03-17 15:46:12 +0800

    Create PongGame.java

---
+++ Pong Game/PongGame.java
@@ -0,0 +1,32 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PongGame implements KeyListener {
+
+    public PongGame(GameFrame gameFrame) {
+    }
+
+    public static void main(String[] args) {
+
+        new MainMenu();
+    
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+}
